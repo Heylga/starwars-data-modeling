16ece3d31f038b8b6f2f03f0ba73542005179e5f
Heylga <oy.golovina@gmail.com>
2022-05-04 18:52:25 +0000

    the db created

--- src/models.py
+++ src/models.py
@@ -8,14 +8,14 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
-class Address(Base):
+class LogIn(Base):
     __tablename__ = 'address'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
@@ -23,8 +23,45 @@ class Address(Base):
     street_name = Column(String(250))
     street_number = Column(String(250))
     post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    login_id = Column(Integer, ForeignKey('login.id'))
+    LogIn = relationship(User)
+
+
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    characters_id = Column(Integer, ForeignKey('characters.id'))
+    planets_id = Column(Integer, ForeignKey('planets.id'))
+    favorites = relationship(User)
+    
+
+class Characters(Base):
+    __tablename__= 'characters'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    gender = Column(String(50))
+    hair_color = Column(String(50))
+    eye_color = Column(String(50))
+    characters = relationship(Favorites)
+
+class Planets(Base):
+    __tablename__= 'planets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    population = Column(String(50))
+    terrain = Column(String(50))
+    planets = relationship(Favorites)
+
+class Starships(Base):
+    __tablename__= 'starships'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    model = Column(String(50))
+    manufacturer = Column(String(50))
+    starships = relationship(Favorites)
+
 
     def to_dict(self):
         return {}
